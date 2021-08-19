b7fcd1f7c149c92895982770d37cc767306306ac
Felipe Lima <felipellima83@gmail.com>
2021-08-19 20:37:06 -0300

    aula03

--- aula/src/main/java/aula02/ServletAgenda.java
+++ aula/src/main/java/aula02/ServletAgenda.java
@@ -12,19 +12,45 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletAgenda
  */
-@WebServlet(name="/ServletAgenda", urlPatterns="/ServletAgenda")
+@WebServlet("/ServletAgenda")
 public class ServletAgenda extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	@Override 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAgenda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-		PrintWriter writer = resp.getWriter(); writer.println("página de cadastro acessada");
-
-		String nome = req.getParameter("nome");
-		String telefone = req.getParameter("telefone"); 
-		String nascimento = req.getParameter("nascimento");
-		
-		}
-
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter saida = response.getWriter();
+		saida.write("<HTML>"
+					+ "<BODY>"
+						+ "<form action=\"/pagina-processa-dados-do-form\" method=\"post\">\r\n"
+						+ "    <div>\r\n"
+						+ "        <label for=\"name\">Nome:</label>\r\n"
+						+ "        <input type=\"text\" id=\"name\" />\r\n"
+						+ "    </div></br>\r\n"
+						+ "    <div>\r\n"
+						+ "        <label for=\"telefone\">Telefone:</label>\r\n"
+						+ "        <input type=\"text\" id=\"telefone\" />\r\n"
+						+ "    </div></br>\r\n"
+						+ "    <div>\r\n"
+						+ "        <label for=\"dt_nasc\">Data de Nascimento:</label>\r\n"
+						+ "        <input type=\"text\" id=\"dt_nasc\" />\r\n"
+						+ "    </div></br>\r\n"
+						+ "    <div class=\"button\">\r\n"
+						+ "        <button type=\"submit\">Enviar sua mensagem</button>\r\n"
+						+ "    </div>\r\n"
+						+ "</form>"
+					+ "</BODY>"
+				+ "</HTML>");
+		saida.close();
+	}
 }

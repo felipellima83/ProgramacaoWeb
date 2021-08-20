e401948c14064ff2b586075f4b825c2a78a148d4
Felipe Lima <felipellima83@gmail.com>
2021-08-19 21:53:08 -0300

    Agenda funcionando

--- aula/src/main/java/aula02/ServletAgenda.java
+++ aula/src/main/java/aula02/ServletAgenda.java
@@ -2,6 +2,8 @@ package aula02;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,48 +11,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletAgenda
- */
 @WebServlet("/ServletAgenda")
 public class ServletAgenda extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletAgenda() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter saida = response.getWriter();
-		saida.write("<HTML>"
-					+ "<BODY>"
-						+ "<form action=\"/pagina-processa-dados-do-form\" method=\"post\">\r\n"
-						+ "    <div>\r\n"
-						+ "        <label for=\"name\">Nome:</label>\r\n"
-						+ "        <input type=\"text\" id=\"name\" />\r\n"
-						+ "    </div></br>\r\n"
-						+ "    <div>\r\n"
-						+ "        <label for=\"telefone\">Telefone:</label>\r\n"
-						+ "        <input type=\"text\" id=\"telefone\" />\r\n"
-						+ "    </div></br>\r\n"
-						+ "    <div>\r\n"
-						+ "        <label for=\"dt_nasc\">Data de Nascimento:</label>\r\n"
-						+ "        <input type=\"text\" id=\"dt_nasc\" />\r\n"
-						+ "    </div></br>\r\n"
-						+ "    <div class=\"button\">\r\n"
-						+ "        <button type=\"submit\">Enviar sua mensagem</button>\r\n"
-						+ "    </div>\r\n"
-						+ "</form>"
-					+ "</BODY>"
-				+ "</HTML>");
-		saida.close();
+	public ServletAgenda() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String telefone = request.getParameter("telefone");
+		String nascimento = request.getParameter("nascimento");
+		response.getWriter().append("O contato "+nome+", telefone "+telefone+" e nascimento "+nascimento+" foi incluído com sucesso!");
 	}
 }
--- aula/src/main/java/aula03/ServletGetPost.java
+++ aula/src/main/java/aula03/ServletGetPost.java
@@ -2,7 +2,6 @@ package aula03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,32 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletGetPost
- */
 @WebServlet("/ServletGetPost")
 public class ServletGetPost extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ServletGetPost() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter saida = response.getWriter();
-		saida.write("<HTML><BODY>Olá!Resposta do método GET</BODY></HTML>");
+		saida.write("<HTML><BODY>Olá! Resposta do método GET</BODY></HTML>");
 		saida.close();
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter saida = response.getWriter();
-		saida.write("<HTML><BODY>Olá!Resposta do método POST</BODY></HTML>");
+		saida.write("<HTML><BODY>Olá! Resposta do método POST</BODY></HTML>");
 		saida.close();
 	}
 
